802cb4edc413d26bd9c67e9043e1ea07c1d2e3d3
Matt Cowski <Matt.Cowski@gmail.com>
2017-07-29 18:11:13 -0700

    adding flex box styled form and video bg

--- src/App.js
+++ src/App.js
@@ -18,11 +18,32 @@ class App extends Component {
           <h2>Hire Freelancers and Contractors directly with Bitcoin and the Blockchain.</h2>
         </div>
         <p className="App-intro">
-          <input type="text" placeholder="What do you need done?" onChange={this.props.onSearch}/>
+          <div className="cover">
+
+            <form className="flex-form">
+              <input onChange={this.props.onSearch} type="search" placeholder="What do you need done?"/>
+              <label htmlFor="from">From</label>
+              <input type="date" name="from"/>
+
+              <label htmlFor="to">To</label>
+              <input type="date" name="to"/>
+
+              <select>
+                <option value="1">$</option>
+                <option value="2">$$</option>
+                <option value="3">$$$</option>
+              </select>
+
+              <input type="submit" value="Search"/>
+
+            </form>
+          </div>
+
           <code>{JSON.stringify(this.props.providers)}</code>
           <h2>{this.props.providers[0] && "Found "+this.props.providers[0].providers.length+" results for: "+this.props.providers[0].category}</h2>
           {this.props.providers[0] && this.props.providers[0].providers.map(pro=><div>{pro}</div>)}
         </p>
+        <video className="background-video" src="https://coverr.co/s3/mp4/a-working-man.mp4" autoPlay loop/>
       </div>
     );
   }
